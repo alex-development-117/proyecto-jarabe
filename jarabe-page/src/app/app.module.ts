6ce4d8c47be9060c10460d431983b1c552220c48
Alejandro QF <tnt_games_development@outlook.com>
2020-11-25 09:23:51 -0600

    Header y menú vertical añadidos.
    Se añadió el header y el menú vertical, además de haber acabado su diseño. También se creó el archivo ap.routes.ts.

--- jarabe-page/src/app/app.module.ts
+++ jarabe-page/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { InventarioComponent } from './components/inventario/inventario.component';
+import { ProveedoresComponent } from './components/proveedores/proveedores.component';
+import { ProductosComponent } from './components/productos/productos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    HeaderComponent,
+    InventarioComponent,
+    ProveedoresComponent,
+    ProductosComponent
   ],
   imports: [
     BrowserModule,
